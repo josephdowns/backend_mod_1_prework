8bbdec02e8bdbc5b3a69c6cbb1e724ef20d752cd
Joseph Downs <jos.w.downs@gmail.com>
2021-10-30 00:06:53 -0600

    Add Section 1

--- section1/exercises/variables.rb
+++ section1/exercises/variables.rb
@@ -1,6 +1,6 @@
 # In the below exercises, write code that achieves
 # the desired result. To check your work, run this
-# file by entering the following command in your terminal: 
+# file by entering the following command in your terminal:
 # `ruby section1/exercises/variables.rb`
 
 # Example: Write code that saves your name to a variable and
@@ -11,49 +11,64 @@ p name
 # Write code that saves the string 'Dobby' to a variable and
 # prints what that variable holds to the terminal:
 house_elf = "Dobby"
-# YOUR CODE HERE
+puts house_elf
 
 # Write code that saves the string 'Harry Potter must not return to Hogwarts!'
 # and prints what that variable holds to the terminal:
-# YOUR CODE HERE
-
+warning_dobby = "Harry Potter must not return to Hogwarts!"
+puts warning_dobby
 # Write code that adds 2 to the `students` variable and
 # prints the result:
 students = 22
 # YOUR CODE HERE
 p students
-
+new_count = students + 2
+puts new_count
 # Write code that subracts 2 from the `students` variable and
 # prints the result:
 # YOUR CODE HERE
 p students
-
+minus_students = students - 2
+puts minus_students
 
 # YOU DO:
-# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`. 
+# Declare three variables, named `first_name`, `is_hungry` and `number_of_pets`.
 # Store the appropriate data types in each.
 # print all three variables to the terminal.
-
+first_name = "Joseph"
+is_hungry = "nope"
+number_of_pets = 0
+puts first_name
+puts is_hungry
+puts number_of_pets
 # IN WORDS:
-# How did you decide to use the data type you did for each of the three variables above? 
+# How did you decide to use the data type you did for each of the three variables above?
 
 # Explain.
-
+#first_name and is_hungry were asking for strings so I put that data type for those variables.
+#number_of_pets was asking for a number so I put an integer as the variable.
 
 # YOU DO:
 # Re-assign the values to the three variables from the previous challenge to different values (but same data type).
 # print all three variables to the terminal.
-
+first_name = "Donny"
+is_hungry = "for sausage"
+number_of_pets = 3
+puts first_name
+puts is_hungry
+puts number_of_pets
 
 # YOU DO:
 # Using the variables below, print the total number of snacks to the terminal:
 healthy_snacks = 6;
 junk_food_snacks = 8;
-
+total_snacks = healthy_snacks + junk_food_snacks
+puts total_snacks
 
 #-------------------
 # FINAL CHECK
 #-------------------
 
-# Did you run this file in your terminal to make sure everything printed out to the terminal 
-  # as you would expect? 
+# Did you run this file in your terminal to make sure everything printed out to the terminal
+  # as you would expect?
+#Yes
--- section1/reflection.md
+++ section1/reflection.md
@@ -2,18 +2,38 @@
 
 1. How did the SuperLearner Article resonate with you? What from this list do you already do? Want to start doing or do more of? Is there anything not on this list, that you would add to it?
 
+I really appreciated the author stating that teaching something was a part of the learning process. Once I feel like I can grasp something is the moment I feel like I can teach someone else. And usually, I end up learning more. I try to read as much as I can. Anything and everything. I would just say that I think a SuperLearner wants to pursue knowledge for knowledge sake. That they value any and all education.
+
 1. How would you print the string `"Hello World!"` to the terminal?
+
+puts "Hello World!"
 
 1. What character is used to indicate comments in a ruby file?
 
+The pound symbol: #
+
 1. Explain the difference between an integer and a float?
+
+An integer is a whole number whereas a float contains a decimal.
 
 1. In the space below, create a variable `animal` that holds the string `"zebra"`
 
+animal = "zebra"
+
 1. How would you print the string `"zebra"` using the variable that you created above?
+
+print animal
 
 1. What is interpolation? Use interpolation to print a sentence using the variable `animal`.
 
+Interpolation uses variables within strings.
+puts "My nephew loves to go to the zoo and see the #{animal}."
+
 1. What method is used to get input from a user?
 
+gets.chomps
+
 1. Name and describe two common string methods:
+
+.capitalize will change the first letter of the string to a capital letter and lowercase the rest
+.downcase will change all the capital letters in a string to lowercase
