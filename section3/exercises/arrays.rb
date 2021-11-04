71694757ce60d27391bd46bd6b7346b459b0f4d8
Joseph Downs <jos.w.downs@gmail.com>
2021-11-04 00:46:44 -0600

    Add exercises and reflection

--- section3/exercises/arrays.rb
+++ section3/exercises/arrays.rb
@@ -15,50 +15,70 @@
 # EXAMPLE: write code below that will print an array of animals.
 # Store the array in a variable.
 animals = ["Zebra", "Giraffe", "Elephant"];
-print animals
+puts animals
+puts '-' * 10
 
 # EXAMPLE: Write code below that will print "Zebra" from the animals array
 # YOUR CODE HERE
-print animals[0]
+puts animals[0]
+puts '-' * 10
 
 # YOU DO: Write code below that will print the number of elements in array of
 # animals from above.
 
-
+puts animals.length
+puts '-' * 10
 # YOU DO: Write code that will reassign the last item in the animals
 # array to "Gorilla"
 
+animals[2] = "Gorilla"
 
 # YOU DO: Write code that will add a new animal (type of your choice) to position 3.
 
+animals.push("Bobcat")
 
 # YOU DO: Write code that will print the String "Elephant" in the animals array
 
-
+animals.push("Elephant")
+puts animals[4]
+puts '-' * 10
 #-------------------
 # PART 2: Foods: Array Methods
 #-------------------
 
 # YOU DO: Declare a variable that will store an an array of at least 4 foods (strings)
 
+foods = ["grains", "veggies", "fruits", "meat"]
 
 # YOU DO: Write code below that will print the number of elements in the array of
 # foods from above.
 
+puts foods.length
+puts '-' * 10
 
 # YOU DO: Write code below that uses a method to add "broccoli" to the foods array and
 # print the changed array to verify "broccoli" has been added
 
+foods.push("broccoli")
+puts foods
+puts '-' * 10
 
 # YOU DO: Write code below that removes the last item of food from the foods array and
 # print the changed array to verify that item has been removed
 
+foods.pop
+puts foods
+puts '-' * 10
 
-# YOU DO: Write code to add 3 new foods to the array. 
+# YOU DO: Write code to add 3 new foods to the array.
   # There are several ways to do this - choose whichever you'd like!
 # Then, print the changed array to verify the new items have been added
 
+foods.push("sugar", "milk", "brownies")
+puts foods
 # YOU DO: Remove the food that is in index position 0.
+
+foods.shift
 
 #-------------------
 # PART 3: Where are Arrays used?
@@ -77,11 +97,9 @@ print animals[0]
 posts = ["image at beach", "holiday party", "adorable puppy", "video of cute baby"];
 
 # YOU DO: Think of a web application you commonly use. Where do you see LISTS utilized, where arrays
-# may be storing data? Come up with 3 examples - they could be from different web applications or 
+# may be storing data? Come up with 3 examples - they could be from different web applications or
 # all from the same one.
 
-# 1:
-# 2:
-# 3:
-
-
+# 1: reddit has subreddits = ["epic fails", "cute pics", "explainlikeimfive"]
+# 2: Youtube has videos = ["Go Easy on Me", "Rocketman", "Super Mario Secrets"]
+# 3: Amazon has items = ["computers", "chairs", "books"]
--- section3/reflection.md
+++ section3/reflection.md
@@ -2,16 +2,46 @@
 
 1. What are two points from the Growth Mindset article and/or video that either resonated with you, or were brand new to you?
 
+* SMART or Setting Specific, Measurable, Achievable, Relevant, and
+Time-bound goals. I feel like most of my life I have been told to make
+lofty goals or they were not count. SMART is a way to grow and obtain
+goals.
+* That mistakes are part of the process. It seems like I punish myself
+whenever I make a mistake. By saying that mistakes are part of the
+process, it takes some pressure off.
+
 1. In which ways do you currently demonstrate a Growth Mindset? In which ways do you _not_?
+
+* I like to stay in my comfort zone and do little things that push me out of that box, not big things that would freak me out. It is similar to the Zone of Proximal Development.
+* I don't like to practice. To have a Growth Mindset it is important to practice your coding skills and then that is how to master it.
 
 1. What is a Hash, and how is it different from an Array?
 
+A hash is a collection similar to an array. But a hash contains two pieces of information associated with each item where an array only has one.
+
 1. In the space below, create a Hash stored to a variable named `pet_store`.  This hash should hold an inventory of items and the number of that item that you might find at a pet store.
 
+````ruby
+pet_store = {
+  "dog food" => 150,
+  "cat chow" => 120,
+  "fish flakes" => 300
+}
+````
 1. Given the following `states = {"CO" => "Colorado", "IA" => "Iowa", "OK" => "Oklahoma"}`, how would you access the value `"Iowa"`?
+
+`states["IA"]`
 
 1. With the same hash above, how would we get all the keys?  How about all the values?
 
+`states.keys`
+`states.values`
+
 1. What is another example of when we might use a hash?  In your example, why is a hash better than an array?
 
+* Say we had a group of first-graders and we wanted to associate each student with their favorite super hero. Using an array we could use the students' names as keys and the super heroes as values.
+* An array would not let us associate the students with the super heroes. Using an array we could either access the student to find their favorite super hero or we could look up the super hero and see who loves them.
+
 1. What questions do you still have about hashes?
+
+None at this time.
